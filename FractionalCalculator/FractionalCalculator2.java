ca8ee6abaca42b13612a36c50ea9bb59fc4fd06b
jacob8ng <jacob8ng@yahoo.com>
2015-11-22 22:45:49 -0800

    FracCalc

--- FractionalCalculator/FractionalCalculator2.java
+++ FractionalCalculator/FractionalCalculator2.java
@@ -26,12 +26,12 @@ public class FractionalCalculator2{
     }
     public static String operand1(String x){
         String a = x.substring(0,x.indexOf(" "));
-        String whole = whole1(a);
-        String num = num1(a);
-        String denom = denom1(a);
+        String whole = whole(a);
+        String num = num(a);
+        String denom = denom(a);
         return "whole:"+whole+" numerator:"+num+" denominator:"+denom;
     }
-    public static String whole1(String a){
+    public static String whole(String a){
         int x = a.indexOf("_");
         int y = a.indexOf("/");
         if(x!=-1){
@@ -42,18 +42,18 @@ public class FractionalCalculator2{
             return a.substring(0,a.length());
         }
     }
-    public static String num1(String a){
+    public static String num(String a){
         int x = a.indexOf("_");
         int y = a.indexOf("/");
         if(x!=-1){
             return a.substring((x+1),y);
         }else if(y!=-1){
-            return a.substring(0,a.indexOf("/"));
+            return a.substring(0,y);
         }else{
             return "0";
         }
     }
-    public static String denom1(String a){
+    public static String denom(String a){
         int x = a.indexOf("_");
         int y = a.indexOf("/");
         if(x!=-1||y!=-1){
@@ -67,40 +67,9 @@ public class FractionalCalculator2{
     }
     public static String operand2(String x){
         String a = x.substring((x.indexOf(" ")+3),x.length());
-        String whole = whole2(a);
-        String num = num2(a);
-        String denom = denom2(a);
+        String whole = whole(a);
+        String num = num(a);
+        String denom = denom(a);
         return "whole:"+whole+" numerator:"+num+" denominator:"+denom;
-    }
-    public static String whole2(String a){
-        int x = a.indexOf("_");
-        int y = a.indexOf("/");
-        if(x!=-1){
-            return a.substring(0,x);
-        }else if(y!=-1){
-            return "0";
-        }else{
-            return a.substring(0,a.length());
-        }
-    }
-    public static String num2(String a){
-        int x = a.indexOf("_");
-        int y = a.indexOf("/");
-        if(x!=-1){
-            return a.substring((x+1),y);
-        }else if(y!=-1){
-            return a.substring(0,a.indexOf("/"));
-        }else{
-            return "0";
-        }
-    }
-    public static String denom2(String a){
-        int x = a.indexOf("_");
-        int y = a.indexOf("/");
-        if(x!=-1||y!=-1){
-            return a.substring((y+1),a.length());
-        }else{
-            return "1";
-        }
     }
 }
